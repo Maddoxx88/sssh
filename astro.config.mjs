2e8364dc65b84713d6449ca07df96296803b9707
Sunit Shirke <sunitshirke88@gmail.com>
2025-03-01 23:51:50 -0500

    feat: worked on nav and header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
   site: 'https://Maddoxx88.github.io',
 })
