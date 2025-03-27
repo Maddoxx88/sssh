22ff2a9c2bbf0861094ea279e1f9ea34ad9db46d
Sunit Shirke <sunitshirke88@gmail.com>
2025-03-26 23:51:03 -0400

    ref: add static and helper data to data and types folders

---
+++ src/data/projects.ts
@@ -0,0 +1,85 @@
+import { ProgrammingTags } from '../types/enums';
+
+export const projects = [
+    {
+        name: "microregex",
+        url: "https://microregex.netlify.app/",
+        role: "co-creator and maintainer",
+        description: "open source containing highly curated catalog of regular expression patterns",
+        tags: [
+            ProgrammingTags.React,
+            ProgrammingTags.PostgreSQL,
+            ProgrammingTags.ChakraUI,
+            ProgrammingTags.GraphQL
+        ],
+    },
+    {
+        name: "digital wallet payment system",
+        url: "https://github.com/Maddoxx88/wallet-payment-network",
+        role: "creator",
+        description: "features include managing accounts, sending and requesting money, and analyzing transaction data. project created as final deliverable for CS631 (db and sys design)",
+        tags: [
+            ProgrammingTags.React,
+            ProgrammingTags.PostgreSQL,
+            ProgrammingTags.ChakraUI,
+            ProgrammingTags.GraphQL
+        ],
+    },
+    {
+        name: "laptop component knowledge base & QA system",
+        url: "https://github.com/Maddoxx88/laptopkb-and-query",
+        role: "creator",
+        description: "an interactive app that classifies laptop components from images. developed a dynamic QA system leveraging a knowledge base",
+        tags: [
+            ProgrammingTags.React,
+            ProgrammingTags.PostgreSQL,
+            ProgrammingTags.ChakraUI,
+            ProgrammingTags.GraphQL
+        ],
+    },
+    {
+        name: "infinity camera for noise reduction",
+        url: "https://github.com/Maddoxx88/vkpro",
+        role: "co-creator",
+        description: "smart camera mobile app for android which minimizes noise reduction in the post processing of a photo using renderscript api (gpu)",
+        tags: [
+            ProgrammingTags.React,
+            ProgrammingTags.PostgreSQL,
+            ProgrammingTags.ChakraUI,
+            ProgrammingTags.GraphQL
+        ],
+    },
+    {
+        name: "excalidraw libraries",
+        url: "https://github.com/Maddoxx88/excalidraw-libraries",
+        role: "contributor",
+        description: "contributed various libraries to the repo",
+        tags: [
+            ProgrammingTags.React,
+            ProgrammingTags.PostgreSQL,
+            ProgrammingTags.ChakraUI,
+            ProgrammingTags.GraphQL
+        ],
+    },
+    {
+        name: "cpu scheduing algos",
+        url: "https://github.com/Maddoxx88/cpu-scheduling-algos",
+        role: "creator",
+        description: "various cpu scheduing algos visualized. used sveltekit.",
+        tags: [ProgrammingTags.SvelteKit],
+    },
+    {
+        name: "ifsc validator",
+        url: "https://github.com/Maddoxx88/ifsc-validator",
+        role: "creator",
+        description: "mobile app to validate ifsc codes using razorpay api",
+        tags: [ProgrammingTags.SvelteKit],
+    },
+    {
+        name: "wishing well",
+        url: "https://wishingwell-ai.vercel.app/",
+        role: "co-creator and developer",
+        description: "ai gift generator",
+        tags: [ProgrammingTags.NextJS, ProgrammingTags.TailwindCSS, ProgrammingTags.Vercel]
+    },
+];
